5b8709bfecd5097315a39d28a7289d5959da4d65
Christian <kristian2015ok@gmail.com>
2024-06-09 14:46:45 +0700

    Add: Making a POST Request

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 const Create = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('mario');
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
-  }
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New Blog added");
+      setIsPending(false);
+    });
+  };
 
   return (
     <div className="create">
@@ -33,7 +43,7 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        <button>{!isPending ? "Add Blog" : "Loading..."}</button>
       </form>
     </div>
   );
